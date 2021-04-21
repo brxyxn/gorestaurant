d6832d0869a3d99151dda31785a7512fbaa87b57
BRAYAN LOPEZ <brxyxn.corp@live.com>
2021-04-21 15:14:00 -0600

    Prepared everything to connect a DB
    Initialized go.mod, added package pgx/v4 and set up main.go with basic structure

---
+++ go.mod
@@ -0,0 +1,11 @@
+module gorestaurant.gt
+
+go 1.16
+
+require (
+	github.com/jackc/pgx v3.6.2+incompatible // indirect
+	github.com/jackc/pgx/v4 v4.11.0
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
+	golang.org/x/text v0.3.6 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/jackc/pgx/v4"
 )
 
+func ConnPG() {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	defer conn.Close(context.Background())
+
+	var greeting string
+	err = conn.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+// Serve HomePage output
+func homePage(w http.ResponseWriter, r *http.Request) {
+	//Output as JSON
+	fmt.Fprintf(w, "Welcome to homePage!")
+}
+
+// Handle Requests
+func handleRequest() {
+	// localhost/
+	http.HandleFunc("/home", homePage)
+	http.ListenAndServe(":3005", nil)
+}
+
 func main() {
-	fmt.Println("Initial commit")
+	handleRequest()
 }
